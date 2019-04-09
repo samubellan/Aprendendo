04944f28bdafd07453da376167355288f8e045a7
Gabriel Arioza <gabriel_alencar95@hotmail.com>
2019-04-09 19:58:16 -0400

    Adiciona ação no botão "ADICIONAR TAREFA", agora ao clicar sobre o botão, sistema insere o texte da tarefa no campo do TextView.

--- app/src/main/java/com/example/aprendendo/TelaInicialActivity.java
+++ app/src/main/java/com/example/aprendendo/TelaInicialActivity.java
@@ -2,12 +2,46 @@ package com.example.aprendendo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class TelaInicialActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    private EditText campoTarefa;
+    //private ListView listaTarefas;
+    private ListView tarefas;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_inicial);
+
+        campoTarefa = findViewById(R.id.etTarefaDigitada);
+        //listaTarefas = findViewById(R.id.tvTarefa);
+        tarefas = findViewById(R.id.listTarefas);
+
+        //Criando um adapter para a lista
+            ArrayAdapter<String> adaptador = new ArrayAdapter<String>(
+                getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                itens
+            );
+
+        //Adicionando o adapter na lista
+        tarefas.setAdapter(adaptador);
+    }
+    private String[] itens[10];
+    //criando ação de botão para inserir uma nova tarefa
+    public void adicionarItens(ListView listView){
+        //String tarefa = campoTarefa.getText().toString();
+        //listaTarefas.setText(tarefa);
+        for (int cont=0; cont<=9 ; cont++){
+            String tarefaDigitada = campoTarefa.getText().toString();
+            itens[cont]=tarefaDigitada;
+        }
     }
 }
