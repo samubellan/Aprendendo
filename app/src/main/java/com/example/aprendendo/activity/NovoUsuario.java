7408c8c545867532ff88d918a541a2f7ba478306
Gabriel Arioza <gabriel_alencar95@hotmail.com>
2019-05-16 22:22:09 -0400

    Implementado parcialmente funcionalidade de validação de dados para cadastro de usuário.

--- app/src/main/java/com/example/aprendendo/activity/NovoUsuario.java
+++ app/src/main/java/com/example/aprendendo/activity/NovoUsuario.java
@@ -4,13 +4,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.aprendendo.R;
 import com.example.aprendendo.model.Usuario;
 import com.example.aprendendo.repositorios.RepositorioUsuarios;
 
+import org.w3c.dom.Text;
+
 public class NovoUsuario extends AppCompatActivity {
+
+    private EditText nome;
+    private EditText sobrenome;
+    private EditText endEletronico;
+    private EditText senha;
+    private EditText confirmacaoSenha;
+
+    private String name;
+    private String lastName;
+    private String email;
+    private String password;
+    private String passwordConfirmation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,22 +33,36 @@ public class NovoUsuario extends AppCompatActivity {
         setContentView(R.layout.activity_novo_usuario);
     }
 
-    public void cadastrarNovoUsuario(View view){
-        EditText name = findViewById(R.id.etxtName);
-        EditText lastName = findViewById(R.id.etxtLastName);
-        EditText email = findViewById(R.id.etxtEmail);
-        EditText password = findViewById(R.id.etxtPassWord);
-        EditText passwordConfirmation = findViewById(R.id.etxtPassWordConfirmation);
+    public void salvarUsuario(View view) {
+        nome = (EditText) findViewById(R.id.etxtName);
+        sobrenome = (EditText) findViewById(R.id.etxtLastName);
+        endEletronico = (EditText) findViewById(R.id.etxtEmail);
+        senha = (EditText) findViewById(R.id.etxtPassWord);
+        confirmacaoSenha = (EditText) findViewById(R.id.etxtPassWordConfirmation);
 
-        String nome = name.getText().toString();
-        String sobrenome = lastName.getText().toString();
-        String endEletronico = email.getText().toString();
-        String senha = password.getText().toString();
-        String confirmaSenha = passwordConfirmation.getText().toString();
+        name = nome.getText().toString();
+        lastName = sobrenome.getText().toString();
+        email = endEletronico.getText().toString();
+        password = senha.getText().toString();
+        passwordConfirmation = confirmacaoSenha.getText().toString();
 
-        Usuario usuario = new Usuario( 1, nome , sobrenome, endEletronico, senha);
-        Toast.makeText(getApplicationContext(), "Usuário cadastrado :)", Toast.LENGTH_SHORT).show();
-        RepositorioUsuarios reposit = RepositorioUsuarios.getDbUsuarios(this);
-        reposit.addUsuario(usuario);
+        if(name.isEmpty() && email.isEmpty() && password.isEmpty() && passwordConfirmation.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Preencha todos os dados obrigatórios!", Toast.LENGTH_LONG).show();
         }
+        else if(name!=null && email != null && password != null && passwordConfirmation != null && password.equals(passwordConfirmation)) {
+            //Usuario usuario = new Usuario(1, nome.getText().toString(), sobrenome.getText().toString(), endEletronico.getText().toString(), senha.getText().toString());
+            Toast.makeText(getApplicationContext(), "Usuário " + name + " cadastrado :)", Toast.LENGTH_LONG).show();
+            //RepositorioUsuarios repositorio = RepositorioUsuarios.getDbUsuarios(this);
+            //repositorio.addUsuario(usuario);
+            finish();
+        }
+    }
+
+
+    public void setUsuario(Usuario usuario) {
+        nome.setText(usuario.getName());
+        sobrenome.setText(usuario.getLastName());
+        endEletronico.setText(usuario.getEmail());
+        senha.setText(usuario.getPassword());
+    }
 }
