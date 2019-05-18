f47bffe42a6a5212116af5ad8558459ae4e92d90
Gabriel Arioza <gabriel_alencar95@hotmail.com>
2019-05-17 23:13:26 -0400

    Foram implementado as seguintes funcionalidades: →TELA DE CADASTRO DE USUÁRIO: •Verificação sobre os dados obrigatórios para cadastro; •Validação de senha (verificar se senha e confirmação de senha são iguais).
    →TELA DE LISTAGEM DE LISTAS:
    •Alterado o layout do adapter, inserindo botão para editar a Lista.

--- app/src/main/java/com/example/aprendendo/activity/NovoUsuario.java
+++ app/src/main/java/com/example/aprendendo/activity/NovoUsuario.java
@@ -1,5 +1,6 @@
 package com.example.aprendendo.activity;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,17 +16,25 @@ import org.w3c.dom.Text;
 
 public class NovoUsuario extends AppCompatActivity {
 
+    //criando variáveis para coletar os dados digitados na activity
     private EditText nome;
     private EditText sobrenome;
     private EditText endEletronico;
     private EditText senha;
     private EditText confirmacaoSenha;
 
+    //criando variáveis para salvar os dados digitados na activity após conversão (.toString)
     private String name;
     private String lastName;
     private String email;
     private String password;
     private String passwordConfirmation;
+
+    //criando variáveis para alteração de cor do texto, para itens obrigatórios
+    private TextView colorName;
+    private TextView colorEmail;
+    private TextView colorPassword;
+    private TextView colorPasswordConfirmation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,36 +42,91 @@ public class NovoUsuario extends AppCompatActivity {
         setContentView(R.layout.activity_novo_usuario);
     }
 
+    //classe utilizada no onClick, configurado no botão "CADASTRAR"
     public void salvarUsuario(View view) {
+        //buscando os EditText pelo findViewById
         nome = (EditText) findViewById(R.id.etxtName);
         sobrenome = (EditText) findViewById(R.id.etxtLastName);
         endEletronico = (EditText) findViewById(R.id.etxtEmail);
         senha = (EditText) findViewById(R.id.etxtPassWord);
         confirmacaoSenha = (EditText) findViewById(R.id.etxtPassWordConfirmation);
 
+        //buscando os TextView pelo findViewById
+        colorName = (TextView) findViewById(R.id.textViewName);
+        colorEmail = (TextView) findViewById(R.id.textViewEmail);
+        colorPassword = (TextView) findViewById(R.id.textViewPassword);
+        colorPasswordConfirmation = (TextView) findViewById(R.id.textViewPasswordConfirmation);
+
+        //converter os dados digitados na Activity em String
         name = nome.getText().toString();
         lastName = sobrenome.getText().toString();
         email = endEletronico.getText().toString();
         password = senha.getText().toString();
         passwordConfirmation = confirmacaoSenha.getText().toString();
 
-        if(name.isEmpty() && email.isEmpty() && password.isEmpty() && passwordConfirmation.isEmpty()) {
-            Toast.makeText(getApplicationContext(), "Preencha todos os dados obrigatórios!", Toast.LENGTH_LONG).show();
+        //inicio do IF e ELSE de verificação dos dados
+        //verificando se os dados obrigatórios foram preenchidos
+        if(name.isEmpty() || email.isEmpty() || password.isEmpty() || passwordConfirmation.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Preencha todos os dados obrigatórios!", Toast.LENGTH_SHORT).show();
+            if(name.isEmpty()) {
+                colorName.setTextColor(Color.RED);
+                if (email.isEmpty()) {
+                    colorEmail.setTextColor(Color.RED);
+                    if (password.isEmpty()) {
+                        colorPassword.setTextColor(Color.RED);
+                        if (passwordConfirmation.isEmpty()) {
+                            colorPasswordConfirmation.setTextColor(Color.RED);
+                        }
+                    }
+                    else if (passwordConfirmation.isEmpty()) {
+                        colorPasswordConfirmation.setTextColor(Color.RED);
+                    }
+                }
+                else if(password.isEmpty()){
+                    colorPassword.setTextColor(Color.RED);
+                    if (passwordConfirmation.isEmpty()) {
+                        colorPasswordConfirmation.setTextColor(Color.RED);
+                    }
+                }
+                else if(passwordConfirmation.isEmpty()){
+                    colorPasswordConfirmation.setTextColor(Color.RED);
+                }
+            }
+            else if(email.isEmpty()) {
+                colorEmail.setTextColor(Color.RED);
+                if (password.isEmpty()) {
+                    colorPassword.setTextColor(Color.RED);
+                    if (passwordConfirmation.isEmpty()) {
+                        colorPasswordConfirmation.setTextColor(Color.RED);
+                    }
+                }
+                else if(passwordConfirmation.isEmpty()){
+                    colorPasswordConfirmation.setTextColor(Color.RED);
+                }
+            }
+            else if(password.isEmpty()) {
+                colorPassword.setTextColor(Color.RED);
+                if (passwordConfirmation.isEmpty()) {
+                    colorPasswordConfirmation.setTextColor(Color.RED);
+                }
+            }
+            else if(passwordConfirmation.isEmpty()) {
+                colorPasswordConfirmation.setTextColor(Color.RED);
+            }
         }
-        else if(name!=null && email != null && password != null && passwordConfirmation != null && password.equals(passwordConfirmation)) {
-            //Usuario usuario = new Usuario(1, nome.getText().toString(), sobrenome.getText().toString(), endEletronico.getText().toString(), senha.getText().toString());
-            Toast.makeText(getApplicationContext(), "Usuário " + name + " cadastrado :)", Toast.LENGTH_LONG).show();
-            //RepositorioUsuarios repositorio = RepositorioUsuarios.getDbUsuarios(this);
-            //repositorio.addUsuario(usuario);
+
+        //verificando se as senhas conhecidem e inserindo os dados no banco
+        else if(password.equals(passwordConfirmation)){
+            Toast.makeText(getApplicationContext(),"Usuário "+name+" cadastrado :)", Toast.LENGTH_SHORT).show();
+            Usuario usuario = new Usuario(1, name, lastName, email, password);
+            RepositorioUsuarios repositorio = RepositorioUsuarios.getDbUsuarios(this);
             finish();
         }
-    }
-
-
-    public void setUsuario(Usuario usuario) {
-        nome.setText(usuario.getName());
-        sobrenome.setText(usuario.getLastName());
-        endEletronico.setText(usuario.getEmail());
-        senha.setText(usuario.getPassword());
+        //mensagem informando que senhas não conhecidem
+        else{
+            Toast.makeText(getApplicationContext(), "As senhas não conhecidem :(", Toast.LENGTH_SHORT).show();
+            colorPassword.setTextColor(Color.RED);
+            colorPasswordConfirmation.setTextColor(Color.RED);
+        }
     }
 }
